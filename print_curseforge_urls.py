d8f65caa2f053d6ffc5eb092c36721e702eec580
Josh Freedman <josh@freedman.dev>
2024-10-29 19:03:44 -0400

    Revive server with new modpack
    * Using neoforge instead of forge
    * Using new set of mods that Amaya picked

--- print_curseforge_urls.py
+++ print_curseforge_urls.py
@@ -1,9 +1,9 @@
-import json;
+import json
 
 for f in json.load(open("manifest.json", "r"))["files"]:
     if not f["required"]:
         continue
     file_id = f["fileID"]
     project_id = f["projectID"]
-    print("https://www.curseforge.com/api/v1/mods/%i/files/%i/download" % (project_id, file_id))
-
+    print("https://www.curseforge.com/api/v1/mods/%i/files/%i/download" %
+          (project_id, file_id))
